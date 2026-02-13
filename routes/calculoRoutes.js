f7bd9a5d8d8013090aaa5200fe4a5f101a1079e3
Yasmin Zaratim <135038867+YasminGZaratim@users.noreply.github.com>
2026-02-13 14:52:44 -0300

    Delete routes directory

--- routes/calculoRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { calcularImpostos } = require('.../controllers/calculoController');
-
-router.post('/', calcularImpostos);
-
-module.exports = router;
